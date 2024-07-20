bc32c1f8933ed828c4c2eaf1178412fac6a9228b
Amit Kumar Mandal <126327335+amitkr069@users.noreply.github.com>
2024-07-20 16:33:48 +0530

    Supporting functions and programs

---
+++ preprocess.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+
+
+def preprocessor(df, region_df):
+
+    df = df[df['Season'] == 'Summer']
+
+    df = df.merge(region_df, on='NOC', how='left')
+
+    df.drop_duplicates(inplace=True)
+
+    df = pd.concat([df, pd.get_dummies(df['Medal']).astype(int)], axis=1)
+    return df
